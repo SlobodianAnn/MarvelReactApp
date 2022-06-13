750cb18b1e55cd6ccf42f96c9e34a4248c76d2d6
Anna Slobodian <anna.slobodian@mediamonks.com>
2022-06-13 21:59:44 +0300

    feat: add routing

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,22 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link, NavLink } from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="#">
+        <Link to="/">
           <span>Marvel</span> information portal
-        </a>
+        </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="#">Characters</a>
+            <Link NavLink exact activeStyle={{ color: '#9f0013' }} to="/">
+              Characters
+            </Link>
           </li>
           /
           <li>
-            <a href="#">Comics</a>
+            <Link NavLink exact activeStyle={{ color: '#9f0013' }} to="/comics">
+              Comics
+            </Link>
           </li>
         </ul>
       </nav>
